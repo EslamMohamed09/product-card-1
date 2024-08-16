4ceef8357aa9d2d24f93422f88dc482d9bb7a599
EslamMayo09 <eslammayo2022@gmail.com>
2024-08-16 21:13:10 +0300

    product card1

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+    $('.small-images > img').click(function(){
+        $('.small-images > img').removeClass('active');
+        $(this).addClass('active');
+        $('.main-image > img').attr('src', $(this).attr('src'));
+        var color = $(this).attr('data-color');
+        $('.text-color h1').text(color).css('color',color);
+        $('.right-content button').css('background',color)
+        $('body').css('background', 'linear-gradient(45deg, white 72%, '+color+' 72%)')
+        $('.right-content h2').text($(this).attr('data-title'))
+    });
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    });
